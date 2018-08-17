b7af0c97ab2533d535007d50f50ec0e49273207f
Momen Yasser <momenysr@gmail.com>
2018-08-17 15:47:23 +0200

    Init

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <div>
+      Welcome in About <nuxt-link to="/">Home</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {}
+</script>
